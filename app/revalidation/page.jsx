c43f41d17cff937d55cd19749191aa4f5b4a763f
littlecm <clittle.norml@gmail.com>
2024-08-07 19:25:14 -0400

    Update page.jsx

--- app/revalidation/page.jsx
+++ app/revalidation/page.jsx
@@ -4,14 +4,14 @@ export const metadata = {
 
 function IframeWidget() {
     return (
-        <div className="bg-white text-neutral-600 card my-6 max-w-2xl">
-            <div className="card-title text-3xl px-8 pt-8">Dealership Inline Banners</div>
-            <div className="card-body py-4">
+        <div className="bg-white text-neutral-600 my-6" style={{ width: '100%', overflow: 'hidden' }}>
+            <div className="text-3xl px-8 pt-8">Dealership Inline Banners</div>
+            <div className="py-4">
                 <iframe 
                     src="https://miai.retool.com/embedded/public/c4ae30c6-59bd-41c6-9c17-a8ebe9044d41" 
                     width="100%" 
-                    height="600px" 
-                    style={{ border: 'none' }}
+                    height="800px" 
+                    style={{ border: 'none', width: '100vw' }}
                     title="Dealership Inline Banners"
                 />
             </div>
